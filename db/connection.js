c9e5ad4128306c18c6a29a58b4e832eb5bf806d8
tim0git <tim.doolan88@icloud.com>
2020-05-06 16:10:10 +0100

    heroku app set up

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,15 @@
+const ENV = process.env.NODE_ENV || 'development';
 const knex = require('knex');
-const dbConfig = require('../knexfile');
+
+const dbConfig =
+  ENV === 'production'
+    ? { client: 'pg', connection: process.env.DATABASE_URL }
+    : require('../knexfile');
 
 module.exports = knex(dbConfig);
+
+// old config file
+/*const knex = require('knex');
+const dbConfig = require('../knexfile');
+
+module.exports = knex(dbConfig);*/
--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,4 @@
+const { DB_URL } = process.env;
 const ENV = process.env.NODE_ENV || 'development';
 
 const baseConfig = {
@@ -24,7 +25,15 @@ const customConfig = {
       // user,
       // password
     }
-  }
+  },
+  production: {
+    connection: {
+      connectionString: 'postgres://kuatcbmxquycxy:0c255198367816055ba60bf1527b45bb04321033fe54cbcf167183ab26aff7dc@ec2-52-44-166-58.compute-1.amazonaws.com:5432/dfjce2t941hnqr',
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
 
 module.exports = { ...customConfig[ENV], ...baseConfig };
